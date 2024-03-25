57e9493305f1a164af764ec03ee523bd28a06e7a
Florian Krohmer <florian.krohmer@bergfreunde.de>
2024-03-25 07:47:32 +0100

    stand nach dem ersten Kata

--- tests/01.spec.ts
+++ tests/01.spec.ts
@@ -1,1 +1,16 @@
 // @ts-check
+import {expect, test} from "@playwright/test";
+
+test('kata-1', async ({page}) => {
+    await page.goto('/');
+
+    await page.getByText('Wonach suchst Du?', ).click()
+    await page.getByPlaceholder('Wonach suchst Du?').fill('Wanderschuhe')
+    await page.getByPlaceholder('Wonach suchst Du?').press('Enter')
+    await page.locator('#product-list .product-item').first().click()
+
+    await page.getByTestId('available-true').first().click()
+    await page.getByTestId('toBasket').click()
+
+    await expect(page.getByTestId('cartItem').first()).toBeVisible()
+})
